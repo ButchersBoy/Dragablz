ab9418d296cc4191c4aaef180a4d0f930d7630e7
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-09-14 15:44:59 +0100

    fix demo: dont use shared resource on controller [skip ci]

--- DragablzDemo/BoundExampleInterTabClient.cs
+++ DragablzDemo/BoundExampleInterTabClient.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Threading;
 using Dragablz;
 
 namespace DragablzDemo
@@ -9,8 +11,8 @@ namespace DragablzDemo
         public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
         {
             var view = new BoundExampleWindow();
-            var model = new BoundExampleModel();
-            view.DataContext = model;
+            var model = new BoundExampleModel();            
+            view.DataContext = model;            
             return new NewTabHost<Window>(view, view.InitialTabablzControl);            
         }
 
