906d4f304e7e87def2eefbaef1a73325eec409af
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-11-17 00:04:17 +0100

    Feature: Use only supported .NET versions

--- DragablzDemo/SimpleViewModel.cs
+++ DragablzDemo/SimpleViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization.Formatters;
 using DragablzDemo.Annotations;
 
 namespace DragablzDemo
@@ -20,22 +19,14 @@ namespace DragablzDemo
             {
                 if (_isSelected == value) return;
                 _isSelected = value;
-#if NET40
-                OnPropertyChanged("IsSelected");
-#else
                 OnPropertyChanged();
-#endif                
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
-#if NET40
-        protected virtual void OnPropertyChanged(string propertyName)
-#else
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-#endif
         {
             var handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
